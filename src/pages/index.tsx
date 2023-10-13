3ac4feda8c16b26b9aeca4ee922d0ab35645ee50
EIJI01 <chanyut281144@gmail.com>
2023-10-13 14:30:27 +0700

    Add button:feat

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,20 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Button from "@/components/Button";
+import { useCallback } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const handleClickReward = useCallback(() => {}, []);
   return (
     <main className="text-black h-full text-xl grid grid-flow-row auto-rows-max">
       <div className="h-40 bg-black/20 flex justify-center items-center rounded-lg"></div>
-      <Button actionLabel="กดเพื่อดูรางวัล >>>" outline />
+      <Button
+        actionLabel="กดเพื่อดูรางวัล >>>"
+        onclick={handleClickReward}
+        customStyle="w-fit mx-auto mt-2 px-3 hover:bg-opacity-50 duration-500 hover:scale-x-110"
+      />
       <div>row3</div>
     </main>
   );
